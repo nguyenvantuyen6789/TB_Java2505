828b814fe60de5ef89ff7b00f381158d66cb48fe
Nguyen Van Tuyen <nguyenvantuyen6789@gmail.com>
2025-06-06 18:53:36 +0700

    tạo getter setter

---
+++ Buoi 06/Demo01_Getter_Setter_Constructor/src/com/data/Student.java
@@ -0,0 +1,13 @@
+package com.data;
+
+public class Student {
+    public String fullName;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
