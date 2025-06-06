43ad9849413db0403aef93b492be30c5879c0f65
Nguyen Van Tuyen <nguyenvantuyen6789@gmail.com>
2025-06-06 19:23:30 +0700

    in gia tri

--- Buoi 06/Demo01_Getter_Setter_Constructor/src/com/data/constructor/Program.java
+++ Buoi 06/Demo01_Getter_Setter_Constructor/src/com/data/constructor/Program.java
@@ -5,6 +5,11 @@ public class Program {
         // tạo 1 đối tượng order data, dùng hàm khởi tạo k tham số
         OrderData orderData = new OrderData();
 
+        // tạo 1 đối tượng order data, dùng hàm khởi tạo có tham số
         OrderData orderData2 = new OrderData(1, "HN", "Ngoc");
+        // in thông tin khách hàng
+        System.out.println("Id: " + orderData2.getId());
+        System.out.println("Address: " + orderData2.getAddress());
+        System.out.println("Customer Name: " + orderData2.getCustomerName());
     }
 }
