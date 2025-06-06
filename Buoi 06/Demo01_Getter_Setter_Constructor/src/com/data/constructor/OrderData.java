2b2666c759401ec98c353ef3d9b31f9237e32bef
Nguyen Van Tuyen <nguyenvantuyen6789@gmail.com>
2025-06-06 19:05:36 +0700

    ham khoi tao khong tham so

---
+++ Buoi 06/Demo01_Getter_Setter_Constructor/src/com/data/constructor/OrderData.java
@@ -0,0 +1,40 @@
+package com.data.constructor;
+
+public class OrderData {
+
+    private int id;
+
+    private String address;
+
+    private String customerName;
+
+    // hàm khởi tạo k tham số
+    public OrderData() {
+        // code chạy ngay khi tạo đối tượng
+        System.out.println("Hàm khởi tạo không tham số đã được gọi");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+}
---
+++ Buoi 06/Demo01_Getter_Setter_Constructor/src/com/data/constructor/Program.java
@@ -0,0 +1,8 @@
+package com.data.constructor;
+
+public class Program {
+    public static void main(String[] args) {
+        // tạo 1 đối tượng order data, dùng hàm khởi tạo k tham số
+        OrderData orderData = new OrderData();
+    }
+}
